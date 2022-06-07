a2d5ef1ed0992596515e37048a0c09c4397ed755
Anna Halushka <annahalushka@MacBook-Air-Anna.local>
2022-06-07 22:04:26 +0100

    Write test cases, add auto generated cypress files

---
+++ cypress/integration/contact-us.js
@@ -0,0 +1,8 @@
+describe("Test contact us form", () => {
+    it("should be able to submit successfully", () => {
+        // code goes here
+    })
+    it("should not be able to submit as all fields are required ", () => {
+        // code goes here
+    })
+})
