8f04609e9c196797f868392488c7c28d6903fd3b
Anna Halushka <annahalushka@MacBook-Air-Anna.local>
2022-06-08 23:03:40 +0100

    Click on contact us

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -1,7 +1,10 @@
+/// <reference types="cypress" />
+
 describe("Test contact us form", () => {
     it("should be able to submit successfully", () => {
-        // code goes here
-    })
+        cy.visit("https://www.webdriveruniversity.com/");
+        cy.get('#contact-us > .thumbnail').click()
+})
     it("should not be able to submit as all fields are required ", () => {
         // code goes here
     })
